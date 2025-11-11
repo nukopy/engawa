f1c271b97e382ffda9271d60554eff657ed42f22
nukopy <nukopy@gmail.com>
2025-11-12 01:37:28 +0900

    refactor: モジュール名の変更

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,8 +1,8 @@
 //! WebSocket chat server implementation.
 
 mod handler;
-mod runner;
+mod server;
 mod signal;
 pub mod state; // UseCase 層からアクセスするため public に変更
 
-pub use runner::run;
+pub use server::run;
